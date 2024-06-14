a495eea1187eb5d7b0d80843c646bd1676cf5ea8
Billzerma <nabilzerma272002@gmail.com>
2024-06-14 13:53:06 +0700

    login sign up sudah

--- app/src/main/java/com/sukasrana/notesapp/view/MainActivity.kt
+++ app/src/main/java/com/sukasrana/notesapp/view/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
+    Text(text = "Hello", modifier = modifier)
 }
 
 @Preview(showBackground = true)
